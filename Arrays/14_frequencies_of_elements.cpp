5a370c90612b2521807d8f89306fd59920c04adc
Sahil <sahilchauhan45448@gmail.com>
2023-01-16 21:22:59 +0530

    fix bug of counting frequencies of elements

--- Arrays/14_frequencies_of_elements.cpp
+++ Arrays/14_frequencies_of_elements.cpp
@@ -5,28 +5,31 @@ I/p : {10, 10, 10, 25, 30, 30}
 O/p : 3, 1, 2
 */
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 void printFreq(int arr[], int n)
 {
-    int freq = 1, i=1;
-    while(i<=n)
+    int freq = 1, i = 1;
+    while (i <= n)
     {
-        while(i<n && arr[i]==arr[i-1])
+        while (i < n && arr[i] == arr[i - 1])
         {
             freq++;
             i++;
         }
-        cout<<arr[i-1]<<" "<<freq<<" \n";
+        cout << arr[i - 1] << " " << freq << " \n";
         freq = 1;
         i++;
     }
+    if (n == 1 || arr[n - 1] != arr[n - 2])
+        cout << arr[n - 1] + " " + 1;
 }
 
-int main(){
+int main()
+{
     int arr[] = {10, 10, 10, 10};
-    int n = sizeof(arr)/sizeof(arr[0]);
+    int n = sizeof(arr) / sizeof(arr[0]);
     printFreq(arr, n);
     return 0;
 }
